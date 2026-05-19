96b70bba0f0518dd733de437c8b60b34feca7da2
Urien <yavuzgunay234@gmail.com>
2026-05-19 23:18:41 +0300

    firewall push

---
+++ BannerlordFirewall/BannerlordFirewall.cs
@@ -0,0 +1,85 @@
+﻿using BannerlordFirewall.MissionBehaviors;
+using BannerlordFirewall.PatchedCode;
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.Diamond;
+using TaleWorlds.PlayerServices;
+using WindowsFirewallHelper;
+
+namespace BannerlordFirewall
+{
+    public class BannerlordFirewall : MBSubModuleBase
+    {
+        public static BannerlordFirewall Instance;
+
+        // Thread çakışmalarını önlemek için merkezi kilit nesnesi
+        public static readonly object FirewallLock = new object();
+
+        // Güvenli sözlük yapısı (Thread-safe)
+        public ConcurrentDictionary<PlayerId, IAddress> WhitelistedIps;
+
+        // --- HATA VEREN DEĞİŞKENİ BURAYA EKLE ---
+        public HarmonyLib.Harmony HarmonyHandle;
+        // ----------------------------------------
+
+        private IFirewallRule _cachedFirewallRule;
+
+        public string GetFirewallRuleName()
+        {
+            int port = Module.CurrentModule.StartupInfo.ServerPort;
+            return "Bannerlord Firewall " + port.ToString();
+        }
+
+        public IFirewallRule GetFirewallRule()
+        {
+            if (this._cachedFirewallRule == null)
+            {
+                this._cachedFirewallRule = FirewallManager.Instance.Rules.SingleOrDefault(r => r.Name == this.GetFirewallRuleName());
+            }
+            return this._cachedFirewallRule;
+        }
+
+        public void CreateFirewallRule()
+        {
+            this._cachedFirewallRule = FirewallManager.Instance.CreatePortRule(
+                FirewallProfiles.Domain | FirewallProfiles.Private | FirewallProfiles.Public,
+                this.GetFirewallRuleName(),
+                FirewallAction.Allow,
+                Convert.ToUInt16(Module.CurrentModule.StartupInfo.ServerPort),
+                FirewallProtocol.UDP
+            );
+            FirewallManager.Instance.Rules.Add(this._cachedFirewallRule);
+        }
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Instance = this;
+
+            // Sözlüğü asenkron yapıya uygun başlatıyoruz
+            this.WhitelistedIps = new ConcurrentDictionary<PlayerId, IAddress>();
+
+            this.HarmonyHandle = new HarmonyLib.Harmony("mentalrob.bannerlordfirewall.bannerlord");
+            var original = typeof(CustomBattleServer).GetMethod("OnClientWantsToConnectCustomGameMessage", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var prefix = typeof(PatchCustomBattleServer).GetMethod("PrefixOnClientWantsToConnectCustomGameMessage", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+            this.HarmonyHandle.Patch(original, prefix: new HarmonyLib.HarmonyMethod(prefix));
+
+            if (this.GetFirewallRule() == null)
+            {
+                Debug.Print("[BannerlordFirewall] FirewallRule " + this.GetFirewallRuleName() + " not found on your server. Creating...", 0, Debug.DebugColor.Red);
+                this.CreateFirewallRule();
+            }
+        }
+
+        public override void OnBeforeMissionBehaviorInitialize(Mission mission)
+        {
+            Debug.Print("[BannerlordFirewall] Trying to add RemoveIpBehavior...", 0, Debug.DebugColor.DarkYellow);
+            mission.AddMissionBehavior(new RemoveIpBehavior());
+        }
+    }
+}
---
+++ BannerlordFirewall/PatchedCode/PatchCustomBattleServer.cs
@@ -0,0 +1,42 @@
+﻿using Messages.FromCustomBattleServerManager.ToCustomBattleServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade.Diamond;
+using WindowsFirewallHelper.Addresses;
+
+namespace BannerlordFirewall.PatchedCode
+{
+    public class PatchCustomBattleServer
+    {
+        public static bool PrefixOnClientWantsToConnectCustomGameMessage(ClientWantsToConnectCustomGameMessage message)
+        {
+            if (BannerlordFirewall.Instance.GetFirewallRule() == null) return true;
+
+            // 1. Oyuncu verilerini güvenli bir şekilde listeye ekliyoruz
+            foreach (PlayerJoinGameData playerData in message.PlayerJoinGameData)
+            {
+                // IP boşsa veya 0.0.0.0 ise atla (Firewall'un herkesi engellemesini veya izin vermesini önler)
+                if (string.IsNullOrEmpty(playerData.IpAddress) || playerData.IpAddress == "0.0.0.0") continue;
+
+                // Parse hatası alıp çökmemesi için TryParse kullanıyoruz
+                if (SingleIP.TryParse(playerData.IpAddress, out SingleIP firewallIp))
+                {
+                    BannerlordFirewall.Instance.WhitelistedIps[playerData.PlayerId] = firewallIp;
+                    Debug.Print("[BannerlordFirewall] " + playerData.IpAddress + " added to whitelisted ip address", 0, Debug.DebugColor.Green);
+                }
+            }
+
+            // 2. BURASI KRİTİK NOKTA: Windows Firewall kuralını kilit (lock) altında güncelliyoruz
+            lock (BannerlordFirewall.FirewallLock)
+            {
+                BannerlordFirewall.Instance.GetFirewallRule().RemoteAddresses = BannerlordFirewall.Instance.WhitelistedIps.Values.ToArray();
+            }
+
+            return true;
+        }
+    }
+}
